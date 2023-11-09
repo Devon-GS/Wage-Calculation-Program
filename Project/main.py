24982130fed4d8a2c83e75e7774eaa0ee844db1f
Devon <ds.pydev@gmail.com>
2023-11-09 15:11:30 +0200

    Add att week two roster times to db and excel

--- Project/main.py
+++ Project/main.py
@@ -1,6 +1,6 @@
-import att_weekone as awo
-import att_clock_weekone as cwo
-import att_times_weekone as two
+import att_roster_times as awo
+import att_clock_times as cwo
+import att_cal_hours as two
 
 # ATTENDENT WEEKONE ROSTER
 # awo.db_init()
@@ -8,11 +8,20 @@ import att_times_weekone as two
 # awo.db_update_badges()
 # awo.db_to_excel()
 
+# ATTENDENT WEEKTWO ROSTER
+# awo.db_atwo_init()
+# awo.db_atwo_update_dates()
+# awo.db_atwo_update_badges()
+# awo.db_atwo_to_excel()
+
 # ATTENDENT WEEKONE CLOCK TIMES
 # cwo.recent_clock()
 # cwo.clock_times_collector()
 # cwo.att_clock_excel()
 
+# ATTENDENT WEEKTWO CLOCK TIMES
+
 # ATTENDENT WEEKONE TIMES CALCULATION
 # two.att_times_weekone()
+# two.att_total_wo_hours()
 
